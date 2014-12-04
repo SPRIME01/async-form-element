3cf4761c4809261491448d556c0474d1d25ede63
Joshua Peek <josh@joshpeek.com>
2014-12-04 14:51:28 -0800

    Test button serialization

--- test/test.js
+++ test/test.js
@@ -19,6 +19,15 @@ function submit(form) {
   }
 }
 
+function click(el) {
+  var event = document.createEvent('Event');
+  event.initEvent('click', true, true);
+  el.dispatchEvent(event);
+  if (event.defaultPrevented === false) {
+    el.click();
+  }
+}
+
 ['form', 'async-form'].forEach(function(formId) {
   module(formId);
 
@@ -308,6 +317,246 @@ function submit(form) {
       equal(lines[1], 'Content-Disposition: form-data; name="bar"; filename=""');
       equal(lines[3], '');
       ok(window.request.headers['content-type'].match('multipart/form-data'), window.request.headers['content-type']);
+    });
+  });
+
+  promiseTest('form GET ignores unclicked buttons', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'GET';
+      form.action = '/foo';
+
+      var input = window.document.createElement('input');
+      input.type = 'submit';
+      input.name = 'a';
+      input.value = '1';
+      form.appendChild(input);
+
+      var button = window.document.createElement('button');
+      button.name = 'b';
+      button.value = '2';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'submit';
+      button.name = 'c';
+      button.value = '3';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'reset';
+      button.name = 'd';
+      button.value = '3';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'button';
+      button.name = 'e';
+      button.value = '4';
+      form.appendChild(button);
+
+      submit(form);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'GET', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form POST ignores unclicked buttons', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'POST';
+      form.action = '/foo';
+
+      var input = window.document.createElement('input');
+      input.type = 'submit';
+      input.name = 'a';
+      input.value = '1';
+      form.appendChild(input);
+
+      var button = window.document.createElement('button');
+      button.name = 'b';
+      button.value = '2';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'submit';
+      button.name = 'c';
+      button.value = '3';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'reset';
+      button.name = 'd';
+      button.value = '3';
+      form.appendChild(button);
+
+      button = window.document.createElement('button');
+      button.type = 'button';
+      button.name = 'e';
+      button.value = '4';
+      form.appendChild(button);
+
+      submit(form);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'POST', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form GET submit by button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'GET';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'GET', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form GET submit by named button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'GET';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      submitButton.name = 'foo';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'GET', 'request method should be "GET"');
+      equal(window.request.url, '/foo?foo=Submit', 'request url should be "/foo?foo=Submit"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form GET submit by named and value button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'GET';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      submitButton.name = 'foo';
+      submitButton.value = 'bar';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'GET', 'request method should be "GET"');
+      equal(window.request.url, '/foo?foo=bar', 'request url should be "/foo?foo=bar"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form POST submit by button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'POST';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'POST', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, '');
+    });
+  });
+
+  promiseTest('form POST submit by named button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'POST';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      submitButton.name = 'foo';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'POST', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, 'foo=Submit');
+    });
+  });
+
+  promiseTest('form POST submit by named and value button click', 3, function() {
+    var ready = QUnit.createFrame();
+
+    return ready().then(function(window) {
+      var form = window.document.getElementById(formId);
+      window.CustomElements.upgrade(form);
+
+      form.method = 'POST';
+      form.action = '/foo';
+
+      var submitButton = window.document.createElement('input');
+      submitButton.type = 'submit';
+      submitButton.name = 'foo';
+      submitButton.value = 'bar';
+      form.appendChild(submitButton);
+
+      click(submitButton);
+      return ready();
+    }).then(function(window) {
+      equal(window.request.method, 'POST', 'request method should be "GET"');
+      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
+      equal(window.request.body, 'foo=bar');
     });
   });
 
