59c0e7dbc83cb8258b60b50d03580e5703d9248f
Joshua Peek <josh@joshpeek.com>
2014-12-05 16:57:05 -0800

    Remove duplicate test

--- test/modules/shared/cancelable.js
+++ test/modules/shared/cancelable.js
@@ -23,30 +23,6 @@
     });
   });
 
-  promiseTest('form GET request with stopped propagation', 4, function() {
-    var ready = QUnit.createFrame();
-
-    return ready().then(function(window) {
-      var form = window.document.getElementById(formId);
-      window.CustomElements.upgrade(form);
-
-      form.method = 'GET';
-      form.action = '/foo';
-
-      form.addEventListener('submit', function(event) {
-        event.stopPropagation();
-      });
-
-      QUnit.submit(form);
-      return ready();
-    }).then(function(window) {
-      equal(window.request.method, 'GET', 'request method should be "GET"');
-      equal(window.request.url.replace('?', ''), '/foo', 'request url should be "/foo"');
-      equal(window.request.body, '');
-      equal(window.request.headers['content-type'], null);
-    });
-  });
-
   promiseTest('form submit prevent default', 2, function() {
     var ready = QUnit.createFrame();
 
